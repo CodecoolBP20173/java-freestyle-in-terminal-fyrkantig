f1b30bacd000efbd38f9604e3e36d56dc1128240
AttilaMuller <muller.attila.aron@gmail.com>
2018-03-13 15:47:45 +0100

    Added Coordinate class to Field

--- src/com/fyrkantig/pacman/Field.java
+++ src/com/fyrkantig/pacman/Field.java
@@ -5,6 +5,7 @@ import com.fyrkantig.term.Terminal;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Field {
@@ -14,7 +15,11 @@ public class Field {
     private FieldObject [][] map;
     private Terminal term = new Terminal();
 
+    private LinkedList<Coordinate> spawnPoints;
+    private Coordinate playerSpawnPoint;
+
     public Field (){
+        map = new FieldObject[rows][columns];
         initMap();
     }
 
@@ -25,30 +30,54 @@ public class Field {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        map = new FieldObject[rows][columns];
         while(sc.hasNextLine()) {
             for (int i=0; i<map.length; i++) {
                 String[] line = sc.nextLine().trim().split(" ");
                 for (int j=0; j<line.length; j++) {
                     switch (line[j]) {
-                        case "0": map[i][j] = new FieldObject(Style.WALL);
+                        case "0":
+                            map[i][j] = new FieldObject(Style.WALL);
                             break;
-                        case "1": map[i][j] = new FieldObject(Style.EMPTY);
+                        case "1":
+                            map[i][j] = new FieldObject(Style.EMPTY);
                             break;
-                        case "2": map[i][j] = new FieldObject(Style.SPAWN);
+                        case "2":
+                            map[i][j] = new FieldObject(Style.EMPTY);
+                            spawnPoints.add(new Coordinate(j, i));
                             break;
+                        case "3":
+                            map[i][j] = new FieldObject(Style.EMPTY);
+                            playerSpawnPoint = new Coordinate(i, j);
                     }
-                    renderObject(i, j);
+                    renderObject(j, i);
                 }
             }
         }
     }
 
     private void renderObject(int x, int y) {
-        Style style = map[x][y].getStyle();
-        term.setFgColor(style.fgColor);
-        term.setBgColor(style.bgColor);
-        term.setChar(x, y, style.chr);
+        Style object = map[y][x].getStyle();
+        term.setFgColor(object.fgColor);
+        term.setBgColor(object.bgColor);
+        term.setChar(x, y, object.chr);
 
+    }
+
+    public LinkedList<Coordinate> getSpawnPoints() {
+        return spawnPoints;
+    }
+
+    public Coordinate getPlayerSpawnPoint() {
+        return playerSpawnPoint;
+    }
+}
+
+class Coordinate {
+    final int x;
+    final int y;
+
+    Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
